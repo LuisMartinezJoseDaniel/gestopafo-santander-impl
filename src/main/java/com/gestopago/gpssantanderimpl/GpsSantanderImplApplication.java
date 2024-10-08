11e2b9e0b57cbfce53c5fc9a3d8c42c5b31d16f5
Jose Daniel Luis Martinez <jose.luis@puntored.mx>
2024-10-08 16:13:57 -0600

    Prepare Project with MySQL and JPA, and Properties

---
+++ src/main/java/com/gestopago/gpssantanderimpl/GpsSantanderImplApplication.java
@@ -0,0 +1,12 @@
+package com.gestopago.gpssantanderimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsSantanderImplApplication {
+    
+    public static void main( String[] args ) {
+        SpringApplication.run(GpsSantanderImplApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gestopago/gpssantanderimpl/GpsSantanderImplApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gestopago.gpssantanderimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsSantanderImplApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+}
